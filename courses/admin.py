724a7abc96dc6fc9a827b3142e35de7f26db03a8
SvetlanaM <svetlana@margetova.eu>
2016-05-30 10:32:24 +0200

    category admin configuration

--- courses/admin.py
+++ courses/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Course, Category
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'active')
+    list_display = ('title')
     list_filter = ('active')
     list_editable = ('title')
     search_fields = ['title']
