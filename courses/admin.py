5ba6c25a1ee1bf64d84673a154e38cbc1d5136e4
SvetlanaM <svetlana@margetova.eu>
2016-05-30 14:35:01 +0200

    last test

--- courses/admin.py
+++ courses/admin.py
@@ -11,7 +11,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('first_name', 'last_name', 'username', 'password1', 'password2', ),
+            'fields': ('first_name', 'last_name',  ),
         }),
     )
 
