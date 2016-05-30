fc5bc56181f0d5527d41e50e1fbe20f8082ea2bd
SvetlanaM <svetlana@margetova.eu>
2016-05-30 11:01:56 +0200

    venue conf

--- venues/admin.py
+++ venues/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from .models import CourseVenue
 
 class VenueAdmin(admin.ModelAdmin):
-    list_display = ('title', ('street_name', 'street_number'), 'city', 'active', )
-    date_hierarchy = ['updated_date']
+    list_display = ('title', 'street_name', 'street_number', 'city', 'active', )
     list_editable = ('street_name', 'street_number', 'city', )
     search_fields = ('title',)
     list_filter = ('active', 'city', )
