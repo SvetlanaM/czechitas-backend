1edb091786cf05371a60f432863cda398f7dc43f
SvetlanaM <svetlana@margetova.eu>
2016-05-31 19:53:46 +0200

    couch added to api

--- courses/serializers.py
+++ courses/serializers.py
@@ -12,6 +12,7 @@ class UserDetailSerializer(serializers.ModelSerializer):
 			'email',
 			'first_name',
 			'last_name',
+              'couch_set',
 		]
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
