c75169748dba0bf6111445fbc6728c441c49065e
SvetlanaM <svetlana@margetova.eu>
2016-06-02 17:18:31 +0200

    a

--- courses/views.py
+++ courses/views.py
@@ -33,7 +33,7 @@ class CategoryListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         timestamp = self.kwargs['timestamp']
-        queryset = Category.audit_log.filter(action_date__gte = timestamp).order_by('action_date').distinct('action_date')
+        queryset = Category.audit_log.filter(action_date__gte = timestamp).order_by('pk').distinct('pk')
         return queryset
 
     serializer_class = CategorySerializer
