567f0f1b1de0d83e8ee7705ffe92ec70b6bdba89
SvetlanaM <svetlana@margetova.eu>
2016-06-02 19:31:46 +0200

    float number

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,7 +6,7 @@ from courses.views import PreparedCourseListAPIView, OpenCourseListAPIView, Cour
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/v1/venues/$', CourseVenueListAPIView.as_view(), name='venue-list'),
-    url(r'^api/v1/categories/(?P<timestamp>.+)/$', CategoryListAPIView.as_view(), name='category-list'),
+    url(r'^api/v1/categories/(?P<timestamp>\d*[.]?\d+)/$', CategoryListAPIView.as_view(), name='category-list'),
     url(r'^api/v1/cities/$', CityListAPIView.as_view(), name='city-list'),
     url(r'^api/v1/courses/prepared/$', PreparedCourseListAPIView.as_view(), name='course-prepared-list'),
     url(r'^api/v1/courses/open/$', OpenCourseListAPIView.as_view(), name='course-open-list'),
