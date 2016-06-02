c874a18b0bf4b7127e60756609aa43734bd10e52
SvetlanaM <svetlana@margetova.eu>
2016-06-02 22:13:29 +0200

    deleted test

--- courses/views.py
+++ courses/views.py
@@ -36,8 +36,8 @@ class CategoryListAPIView(MultipleModelAPIView):
 
     def get_queryList(self):
         timestamp = float(self.kwargs['timestamp'])
-        timestamp = timestamp - timedelta(hours = 2)
-        date_value = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%S%Z')
+        date_value = datetime.datetime.fromtimestamp(timestamp) - timedelta(hours = 2).strftime('%Y-%m-%dT%H:%M:%S%Z')
+        
 
 
         queryList = (
