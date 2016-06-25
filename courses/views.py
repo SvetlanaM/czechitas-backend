7d76a4a283d4153eb6baf56d9b129eda16c18f9d
SvetlanaM <svetlana@margetova.eu>
2016-06-25 15:23:22 +0200

    update

--- courses/views.py
+++ courses/views.py
@@ -33,6 +33,7 @@ class CategoryListAPIView(generics.ListAPIView):
     def get_queryset(self):
         timestamp = float(self.kwargs['timestamp'])
         date_value = datetime.datetime.fromtimestamp(timestamp) - timedelta(hours = 2)
+        serializer_class = CourseDetailSerializer
         date_value = date_value.strftime('%Y-%m-%dT%H:%M:%S%Z')
         queryset = Course.objects.filter(updated_date__gte = date_value).order_by('updated_date').distinct()
         return queryset
